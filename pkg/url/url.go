17d0615254a87a0b85399a78ea043afe2e712148
chujungeng <chujungeng@gmail.com>
2022-08-07 00:04:25 -0400

    bumped Go version to 1.19

--- pkg/url/url.go
+++ pkg/url/url.go
@@ -1,12 +1,16 @@
 package url
 
 import (
-	"fmt"
-	"path"
-	"strings"
+	"log"
+	"net/url"
 )
 
 func Join(base string, paths ...string) string {
-	p := path.Join(paths...)
-	return fmt.Sprintf("%s/%s", strings.TrimRight(base, "/"), strings.TrimLeft(p, "/"))
+	p, err := url.JoinPath(base, paths...)
+	if err != nil {
+		log.Println(err)
+		return p
+	}
+
+	return p
 }
